5496994e3938507f5b3aba22d5fd2e2a51921054
bilionok <photoshoper16@gmail.com>
2021-05-30 20:30:29 +0300

    first commit

---
+++ 8_1_8/8_1_8.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
+
+void Change(string& s);
+
+int main()
+{
+	string str; 
+	cout << "Enter string:" << endl;
+	getline(cin, str); 
+	Change(str); 
+	cout << "Result: " << str << endl;
+}
+
+void Change(string& str)
+{
+	for (size_t i = 0; i < str.length() - 1; i++) 
+	{
+		if (str[i] == 'n' && str[i + 1] == 'o') {
+			str.replace(i, 2, "***"); 
+			i += 1; 
+		}
+		if (str.length() < 2) {
+			break;
+		}
+	}
+}
